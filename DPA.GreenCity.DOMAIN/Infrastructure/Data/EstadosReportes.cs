e5aefcdb6dc7d12a8550a3db1ea91f65774f155b
William Cutimanco <wcutimanco@gmail.com>
2024-10-01 22:33:32 -0500

    Creación de entidades

---
+++ DPA.GreenCity.DOMAIN/Infrastructure/Data/EstadosReportes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DPA.GreenCity.DOMAIN.Infrastructure.Data;
+
+public partial class EstadosReportes
+{
+    public int IdEstado { get; set; }
+
+    public int? IdReporte { get; set; }
+
+    public string? EstadoAnterior { get; set; }
+
+    public string? EstadoNuevo { get; set; }
+
+    public DateTime? FechaCambio { get; set; }
+
+    public string? ComentarioAdm { get; set; }
+
+    public virtual Reportes? IdReporteNavigation { get; set; }
+}
---
+++ DPA.GreenCity.DOMAIN/Infrastructure/Data/Usuarios.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DPA.GreenCity.DOMAIN.Infrastructure.Data;
+
+public partial class Usuarios
+{
+    public int IdUsuario { get; set; }
+
+    public string Nombre { get; set; } = null!;
+
+    public string Correo { get; set; } = null!;
+
+    public string Contraseña { get; set; } = null!;
+
+    public string TipoUsuario { get; set; } = null!;
+
+    public string? Telefono { get; set; }
+
+    public string? Direccion { get; set; }
+
+    public virtual ICollection<Comentarios> Comentarios { get; set; } = new List<Comentarios>();
+
+    public virtual ICollection<Informes> Informes { get; set; } = new List<Informes>();
+
+    public virtual ICollection<Reportes> Reportes { get; set; } = new List<Reportes>();
+}
